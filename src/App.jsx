a2e7b2f1694c0acd6f67b0dbaa004b8a8815d9b5
Hoang <anhhoang9cpsm@gmail.com>
2026-03-19 01:32:11 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Fields from "./pages/Fields.jsx";
+import Booking from "./pages/Booking.jsx";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/fields" element={<Fields />} />
+        <Route path="/booking" element={<Booking />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
